65322614e1400c8c55326a8fbc332c5a038d9694
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2023-03-27 21:31:54 -0500

    Update Home.js

--- htrllp-final-product/src/Home.js
+++ htrllp-final-product/src/Home.js
@@ -12,6 +12,7 @@ export default function Home() {
           specifically how it is incorporated in works across various domains.
         </h1>
       </div>
+      <p>Chapter 22 of “How to Read Literature Like a Professor” is concerned with the use of blindness in literature. Given that sight is such an incredibly important sense, depriving a character of it has significant implications on their descriptions and interactions within the work. As such, it is important to recognize what questions readers need to ask about a piece of literature rather than determining precise answers. Generally, the motif of blindness is used to reveal hidden truths or convey innocence. Blindness also must follow the Indiana Jones Principle, which states that an important quality about a character needs to be introduced early on before it can affect the plot later in the text. In sum, blindness, both literal and metaphorical, can be used in various ways to reveal pervasive insights about a character’s ignorance, denial, or innocence.</p>
     </div>
   );
 }
