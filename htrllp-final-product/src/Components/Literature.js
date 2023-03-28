a0377db0ffb066972d64afc2dd899ebad9831388
Archytas3435 <73038172+Archytas3435@users.noreply.github.com>
2023-03-27 21:12:56 -0500

    Update Literature.js

--- htrllp-final-product/src/Components/Literature.js
+++ htrllp-final-product/src/Components/Literature.js
@@ -12,5 +12,7 @@ export default function Literature() {
     >
       Literature Home Page
     </h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/Lady_Macbeth_Cattermole.jpg" alt="Lady Macbeth and Duncan" />
+    <p>Shakespeare’s tragedy “Macbeth” reflects the idea of blindness in multiple characters. Macbeth starts off quite weary of the lengths he must go to become Thane of Cawdor, and then King, but becomes increasingly blinded by his lust for power, ultimately leading to his downfall. Lady Macbeth takes an almost opposite journey, originally blinded by the idea of being queen, but increasingly aware and resistant of the violence necessary for Macbeth to become and stay king. The original king, King Duncan, also embodies the theme of blindness as he is blind to the true nature of Macbeth and the danger he poses. Despite Macbeth’s need for power, Duncan is blinded by his belief in honor and relationships, and is killed as a result. This example of blindness by innocence emphasizes the idea that blindness can have tragic consequences.</p>
   );
 }
