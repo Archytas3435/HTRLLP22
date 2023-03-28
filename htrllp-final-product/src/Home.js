c1b90f7c1571a31f5a04126d462ce8b7f2f878c7
SXV357 <shrevis2018@gmail.com>
2023-03-27 23:29:35 -0500

    styling for visual arts page + visibility logic

--- htrllp-final-product/src/Home.js
+++ htrllp-final-product/src/Home.js
@@ -11,22 +11,22 @@ export default function Home() {
           The exhibits are focused on the theme of blindness, but more
           specifically how it is incorporated in works across various domains.
         </h1>
+        <p className="home-page-header">
+          Chapter 22 of “How to Read Literature Like a Professor” is concerned
+          with the use of blindness in literature. Given that sight is such an
+          incredibly important sense, depriving a character of it has
+          significant implications on their descriptions and interactions within
+          the work. As such, it is important to recognize what questions readers
+          need to ask about a piece of literature rather than determining
+          precise answers. Generally, the motif of blindness is used to reveal
+          hidden truths or convey innocence. Blindness also must follow the
+          Indiana Jones Principle, which states that an important quality about
+          a character needs to be introduced early on before it can affect the
+          plot later in the text. In sum, blindness, both literal and
+          metaphorical, can be used in various ways to reveal pervasive insights
+          about a character&aposs ignorance, denial, or innocence.
+        </p>
       </div>
-      <p>
-        Chapter 22 of “How to Read Literature Like a Professor” is concerned
-        with the use of blindness in literature. Given that sight is such an
-        incredibly important sense, depriving a character of it has significant
-        implications on their descriptions and interactions within the work. As
-        such, it is important to recognize what questions readers need to ask
-        about a piece of literature rather than determining precise answers.
-        Generally, the motif of blindness is used to reveal hidden truths or
-        convey innocence. Blindness also must follow the Indiana Jones
-        Principle, which states that an important quality about a character
-        needs to be introduced early on before it can affect the plot later in
-        the text. In sum, blindness, both literal and metaphorical, can be used
-        in various ways to reveal pervasive insights about a character’s
-        ignorance, denial, or innocence.
-      </p>
     </div>
   );
 }
